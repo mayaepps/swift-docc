9cd3de614df18bf6bd9045f5bf754c46f57099ce
Maya Epps <mayaepps@Mayas-MacBook-Pro-2.local>
2022-06-01 19:10:00 -0700

    Encodes differences between the kind property and the abstract in RenderNodes as JSONPatches. Also adds the old RenderNode being compared and the patches being accumulated to the UserInfo in the encoder.

---
+++ Sources/SwiftDocC/Model/Rendering/Diffing/VersionDifferences.swift
@@ -0,0 +1,72 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See https://swift.org/LICENSE.txt for license information
+ See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+
+import Foundation
+
+/// Defines differences between documentation content versions.
+///
+/// This class can be used to accumulate difference information in the form of a JSONPatch while encoding a tree of objects.
+///
+/// ## Topics
+///
+/// ### Applying Patches
+///
+/// - ``RenderNodeVariantOverridesApplier``
+public class VersionDifferences: Codable {
+
+    public var versions = [VersionPatch]()
+
+    public var isEmpty: Bool { versions.isEmpty }
+    
+    public init(values: [VersionPatch] = []) {
+        add(contentsOf: values)
+    }
+    
+    public func add<Patches>(
+        contentsOf patches: Patches
+    ) where Patches: Collection, Patches.Element == VersionPatch {
+        for patch in patches {
+            add(patch)
+        }
+    }
+
+    public func add(_ versionPatch: VersionPatch) {
+        versions.append(versionPatch)
+    }
+}
+
+public class VersionPatch: Codable {
+        
+    public var versionID: String
+        
+    public var displayName: String
+        
+    public var patch: JSONPatch
+    
+    public func add(_ patchOperation: JSONPatchOperation) {
+        patch.append(patchOperation)
+    }
+    
+    public func add<PatchOperations>(
+        contentsOf patchOperations: PatchOperations
+    ) where PatchOperations: Collection, PatchOperations.Element == JSONPatchOperation {
+        for operation in patchOperations {
+            add(operation)
+        }
+    }
+    
+    init(id: String, name: String, jsonPatch: JSONPatch) {
+        versionID = id
+        displayName = name
+        patch = jsonPatch
+    }
+        
+}
